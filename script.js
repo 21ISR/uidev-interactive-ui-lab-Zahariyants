ca36e7a313f06b16ff8c90909c31e1bbcfac0452
incell <vverhtormaskami@gmail.com>
2026-06-05 06:22:35 +0000

    inter

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const popup = document.querySelector('.popup-overlay')
-const btn = document.querySelector('.show-popup')
+const popup = document.querySelector('.popup-overlay');
+const btn = document.querySelector('.show-popup');
+const btncl = document.querySelector('.popup-close');
+const form = document.querySelector('.popup-form');
 
 btn.addEventListener('click', () => {
     popup.classList.add('show');
+});
+
+btncl.addEventListener('click', () => {
+    popup.classList.remove('show');
 });
 
 popup.addEventListener('click', (event) => {
     if (event.target === popup) {
         popup.classList.remove('show');
     }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert("Сообщение отправлено");
+    popup.classList.remove('show');
+    from.reset();
+});
+
+const headers = document.querySelectorAll('.accordion-header')
+headers.forEach((header) => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling
+        const isOpen = header.classList.contains('active')
+        if (isOpen) {
+            content.style.maxHeight = null
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px'
+        }
+        header.classList.toggle('active')
+    })
 });
